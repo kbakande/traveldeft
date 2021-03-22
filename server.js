a3d26d9343f00058b9fb60a6358eec0d0153d91a
koakande <koakande@gmail.com>
2021-03-22 08:18:47 +0000

    add vanilla express server app

---
+++ server.js
@@ -0,0 +1,42 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// // load environment variables
+// require('dotenv').config();
+// const baseUrl = "api.openweathermap.org/data/2.5/weather?";
+// const api = process.env.OPENWEATHER;
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 3000; //create port variable
+
+const server = app.listen(port, () => console.log(`The app is running on port: ${port}`));
+
+// add a get route for data return
+app.get('/getData', (req, res) => res.send(projectData));
+
+// add a post route to add data
+app.post('/postData', (req, res) => {
+    projectData["temperature"] = req.body.temperature;
+    projectData["date"] = req.body.date;
+    projectData['user response'] = req.body["user response"];
+    res.send("Data has been added");
+});
