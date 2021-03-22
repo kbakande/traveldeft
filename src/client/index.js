740902395ceaa016769e6a79cf768ec5f0612803
koakande <koakande@gmail.com>
2021-03-22 15:34:31 +0000

    import the styles and scripts, and export the scripts

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import { application } from './js/formHandler';
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/main.scss';
+import './styles/header.scss';
+
+export {
+    application
+}
