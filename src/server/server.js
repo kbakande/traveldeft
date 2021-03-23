87c4b3f4a56cebf2515dd057aaa4034b6f1fc5ca
koakande <koakande@gmail.com>
2021-03-23 08:10:08 +0000

    modifying the asset route

--- src/server/server.js
+++ src/server/server.js
@@ -13,6 +13,6 @@ app.use(express.static("dist"));
 // configure the port
 const port = 8080;
 
-const server = app.listen(port, () => console.log | (`The app is listening on port: ${port}`));
+const server = app.listen(port, () => console.log(`The app is listening on port: ${port}`));
 
 app.get('/', (req, res) => res.sendFile('dist/index.html'));
