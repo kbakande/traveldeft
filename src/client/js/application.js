b4b6d7e58f294165277e8c6f2d89ae7e8bb76ca5
koakande <koakande@gmail.com>
2021-03-24 03:57:52 +0000

    add functions for webpack testing

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,7 +1,19 @@
+// for testing purposes
 const testScript = async (e) => {
     e.preventDefault();
+    console.log(e.target);
     alert("I am working");
 };
 
+// set the min attribute in date picker
+const greyPastDate = e => {
+    const currentDate = new Date().toISOString().slice(0, 10);
+    e.target.min = currentDate;
+}
+
+// for fetching data from geonames
+const fetchGeoNames = async (e) => {
+    e.preve
+}
 
 export { testScript };
