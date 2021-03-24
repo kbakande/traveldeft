caf156984ebde94af6e40e385164489bf382a92e
koakande <koakande@gmail.com>
2021-03-24 11:26:46 +0000

    adds scripts to display trip info

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -8,8 +8,8 @@ const testScript = async event => {
 
 
 // get day difference between two dates
-const dateDiffInDays = (dateOne, dateTwo) => {
-    const firstDate = new Date(dateOne), secondDate = new Date(dateTwo);
+const dateDiffInDays = (laterDate) => {
+    const firstDate = new Date(new Date().toISOString().slice(0, 10)), secondDate = new Date(laterDate);
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
     // Discard the time and time-zone information.
@@ -25,24 +25,33 @@ const greyPastDate = e => {
     const currentDate = new Date().toISOString().slice(0, 10);
     document.getElementById("date").setAttribute("min", `${currentDate}`);
     document.getElementById("date").setAttribute("placeholder", `${currentDate}`);
+    const formEl = document.querySelector(".input-holder").querySelectorAll("input");
+    const cityName = formEl[0].value;
+    const departureDate = formEl[1].value;
 };
 
 // for fetching data from geonames
 const getCountryData = async event => {
     event.preventDefault();
     const formEl = event.currentTarget.querySelectorAll("input");
-    const country = formEl[0].value;
-    const apiURI = `http://api.geonames.org/searchJSON?q=${country}&maxRows=10&username=koakande`;
+    const cityName = formEl[0].value;
+    const departureDate = formEl[1].value;
+    const days2departure = dateDiffInDays(departureDate);
+    const apiURI = `http://api.geonames.org/searchJSON?q=${cityName}&maxRows=10&username=koakande`;
     const geoNamesStream = await fetch(apiURI);
 
     try {
         const geoNamesObj = await geoNamesStream.json();
         const geoNamesData = {
+            "City": geoNamesObj["geonames"][0]["toponymName"],
             "Country": geoNamesObj["geonames"][0]["countryName"],
             "Latitude": geoNamesObj["geonames"][0]["lat"],
-            "Longitude": geoNamesObj["geonames"][0]["lng"]
+            "Longitude": geoNamesObj["geonames"][0]["lng"],
+            "departureDate": departureDate,
+            "daysToDeparture": days2departure
         };
-        console.log(geoNamesData);
+        // console.log(geoNamesObj, cityName, departureDate, days2departure);
+        displayInfo(geoNamesData)
         return geoNamesData;
     } catch (error) {
         console.log(`error: ${error}`);
@@ -51,14 +60,15 @@ const getCountryData = async event => {
 };
 
 // display the retrieved data from geonames
-const displayInfo = async retrievedData => {
-
+const displayInfo = retrievedData => {
+    document.getElementById("destination").innerText = `My trip to: ${retrievedData["City"]}, ${retrievedData["Country"]}`
+    document.getElementById("departure-day").innerText = `Departing: ${retrievedData["departureDate"]}`
+    document.getElementById("days-away").innerText = `${retrievedData["City"]}, ${retrievedData["Country"]} is ${retrievedData["daysToDeparture"]} days away`
 }
 
 
 export {
     testScript,
-    dateDiffInDays,
     greyPastDate,
-    getCountryData
+    getCountryData,
 };
