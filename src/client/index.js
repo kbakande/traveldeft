441e5ddb1814fee38fe1cdab07824700a8d2bd9e
koakande <koakande@gmail.com>
2021-03-24 08:09:02 +0000

    adds functionality for form input

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 // import scripts
-import { testScript, greyPastDate } from './js/application';
+import { testScript, greyPastDate, getCountryData } from './js/application';
 
 
 // import styles
@@ -8,21 +8,27 @@ import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/main.scss';
 import './styles/header.scss';
+import './styles/form.scss';
 
 // import medias
 import Paris from './media/Paris.jpeg';
 import Logo from './media/logo.png';
 
+// attach the images to the corresponding elements
 document.getElementById("dest-imag").src = Paris;
 document.getElementById("logo").src = Logo;
 
+// testing
 document.getElementById("svTrip").addEventListener("click", testScript);
 
 // grey out past date for date picker input
-greyPastDate()
+greyPastDate();
+
+// test fetch
+document.getElementById("generate").addEventListener("click", getCountryData);
+
 
 
 export {
     testScript
 }
-// http://api.geonames.org/searchJSON?q=london&maxRows=10&username=demo
