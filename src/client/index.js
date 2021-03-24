1cd2a7f41e444cdcc9966fd05cfc4c5ea9c36046
koakande <koakande@gmail.com>
2021-03-24 02:04:06 +0000

    adds images

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,19 @@
+// import scripts
 import { testScript } from './js/application';
+
+// import styles
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/main.scss';
 import './styles/header.scss';
+
+// import medias
+import Paris from './media/Paris.jpeg';
+import Logo from './media/logo.png';
+
+document.getElementById("dest-imag").src = Paris;
+document.getElementById("logo").src = Logo;
 
 export {
     testScript
