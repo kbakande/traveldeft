6b833f19583584fff53731db23ee4bbf611f2e20
koakande <koakande@gmail.com>
2021-03-24 08:09:55 +0000

    adds form control script

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,22 +1,48 @@
+// define variables
+
+
 // for testing purposes
-const testScript = async (e) => {
-    e.preventDefault();
-    console.log(e.target);
-    alert("I am working");
+const testScript = async event => {
+    event.preventDefault();
+    console.log("I am working");
 };
 
 // set the min attribute in date picker
 const greyPastDate = e => {
     const currentDate = new Date().toISOString().slice(0, 10);
     document.getElementById("date").setAttribute("min", `${currentDate}`);
-}
+};
 
 // for fetching data from geonames
-const fetchGeoNames = async (e) => {
-    e.preventDefault();
-}
+const getCountryData = async event => {
+    // event.preventDefault();
+    const countryTest = event;
+    console.log(countryTest);
+    const country = "Lagos";
+    const apiURI = `http://api.geonames.org/searchJSON?q=${country}&maxRows=10&username=koakande`;
+    const geoNamesStream = await fetch(apiURI);
+
+    try {
+        const geoNamesObj = await geoNamesStream.json();
+        const geoNamesData = {
+            "Country": geoNamesObj["geonames"][0]["countryName"],
+            "latitude": geoNamesObj["geonames"][0]["lat"],
+            "longitude": geoNamesObj["geonames"][0]["lng"]
+        };
+        console.log(geoNamesData);
+    } catch (error) {
+        console.log(`error: ${error}`);
+
+    }
+
+
+};
+
+// get country from input field
+
 
 export {
     testScript,
-    greyPastDate
+    greyPastDate,
+    getCountryData
 };
