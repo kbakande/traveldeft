a3286dacf009c96ad12cb2f8ad1a14bc3ea147d5
koakande <koakande@gmail.com>
2021-03-25 20:16:09 +0000

    separates starting of server in the file

---
+++ src/server/start.js
@@ -0,0 +1,6 @@
+const app = require("./server.js");
+const allWeatherData = {};
+// configure the port
+const port = 8081;
+
+const server = app.listen(port, () => console.log(`The app is listening on port: ${port}`));
