c4068c7c90c50304c776825a8a809fb85bf0262e
koakande <koakande@gmail.com>
2021-03-25 20:22:20 +0000

    install supertest

---
+++ __test__/server.test.js
@@ -0,0 +1,28 @@
+const app = require('../src/server/server'); // Link to your server file
+const supertest = require('supertest');
+const request = supertest(app);
+import 'regenerator-runtime/runtime'
+
+it('Gets the getData endpoint', async done => {
+    // Sends GET Request to /test endpoint
+    const res = await request.get('/getData')
+
+    // ...
+    done()
+});
+
+it('gets the getData endpoint', async done => {
+    const response = await request.get('/getData')
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({})
+    done()
+})
+
+it('posts to the postData endpoint', async done => {
+    const response = await request.post('/postData')
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ "success": true })
+    done()
+})
