827d4650aa345393a7d123c7501f1ba339f2ce48
koakande <koakande@gmail.com>
2021-03-25 05:48:04 +0000

    separating front end from backend

--- src/client/js/application.js
+++ src/client/js/application.js
@@ -1,5 +1,5 @@
 // define variables
-
+const geoNamesData = {};
 // for testing purposes
 const testScript = async event => {
     event.preventDefault();
@@ -26,9 +26,6 @@ const greyPastDate = e => {
     const currentDate = new Date().toISOString().slice(0, 10);
     document.getElementById("date").setAttribute("min", `${currentDate}`);
     document.getElementById("date").setAttribute("placeholder", `${currentDate}`);
-    const formEl = document.querySelector(".input-holder").querySelectorAll("input");
-    const cityName = formEl[0].value;
-    const departureDate = formEl[1].value;
 };
 
 // for fetching data from geonames
@@ -42,25 +39,23 @@ const getCountryData = async event => {
     const geoNamesStream = await fetch(apiURI);
     try {
         const geoNamesObj = await geoNamesStream.json();
-        const geoNamesData = {
-            "City": geoNamesObj["geonames"][0]["toponymName"],
-            "Country": geoNamesObj["geonames"][0]["countryName"],
-            "Latitude": geoNamesObj["geonames"][0]["lat"],
-            "Longitude": geoNamesObj["geonames"][0]["lng"],
-            "departureDate": departureDate,
-            "daysToDeparture": days2departure
-        };
-        // console.log(geoNamesObj, cityName, departureDate, days2departure);
-        displayInfo(geoNamesData);
+        geoNamesData["City"] = geoNamesObj["geonames"][0]["toponymName"];
+        geoNamesData["Country"] = geoNamesObj["geonames"][0]["countryName"];
+        geoNamesData["Latitude"] = geoNamesObj["geonames"][0]["lat"];
+        geoNamesData["Longitude"] = geoNamesObj["geonames"][0]["lng"];
+        geoNamesData["departureDate"] = departureDate;
+        geoNamesData["daysToDeparture"] = days2departure;
+
+        // displayInfo(geoNamesData);
+        gePixaBayImg(geoNamesData);
         getWeatherBitData(geoNamesData);
-        gePixaBayImg(geoNamesData["City"]);
-        return geoNamesData;
+
     } catch (error) {
         console.log(`error: ${error}`);
-
     }
 };
 
+// get data from weathherBit
 const getWeatherBitData = async geoData => {
     const lat = await geoData["Latitude"];
     const lon = await geoData["Longitude"];
@@ -75,7 +70,9 @@ const getWeatherBitData = async geoData => {
             const bitData = await weatherbitData.json();
             geoData["high_temp"] = bitData["data"][0]["high_temp"];
             geoData["low_temp"] = bitData["data"][0]["low_temp"];
+            geoData["description"] = bitData["data"][0]["weather"]["description"];
             console.log(geoData);
+            displayInfo(geoData);
         } catch (error) {
             console.log(`error: ${error}`)
         }
@@ -88,14 +85,17 @@ const getWeatherBitData = async geoData => {
         try {
             const bitData = await weatherbitData.json();
             geoData["temp"] = bitData["data"][0]["temp"];
-            console.log(geoData);
+            geoData["description"] = bitData["data"][0]["weather"]["description"];
+            displayInfo(geoData);
         } catch (error) {
             console.log(`error: ${error}`)
         }
     }
 };
 
-const gePixaBayImg = async pixCityName => {
+// get image from pixaBay
+const gePixaBayImg = async geoNamesData => {
+    const pixCityName = geoNamesData["City"];
     const pixBayAPI = "20843692-350cf2ac1c16bbb7ec469b3f2";
     const pixBayURL = `https://pixabay.com/api/?key=${pixBayAPI}&q=${pixCityName}`;
     const pixBayStream = await fetch(pixBayURL);
@@ -103,6 +103,7 @@ const gePixaBayImg = async pixCityName => {
     try {
         const pixBayData = await pixBayStream.json();
         const cityImgLink = pixBayData["hits"][0]["webformatURL"];
+        geoNamesData["CityImg"] = cityImgLink;
         return cityImgLink;
     } catch (error) {
         console.log(`error:${error}`);
@@ -111,9 +112,19 @@ const gePixaBayImg = async pixCityName => {
 
 // display the retrieved data from geonames
 const displayInfo = retrievedData => {
-    document.getElementById("destination").innerText = `My trip to: ${retrievedData["City"]}, ${retrievedData["Country"]}`
-    document.getElementById("departure-day").innerText = `Departing: ${retrievedData["departureDate"]}`
-    document.getElementById("days-away").innerText = `${retrievedData["City"]}, ${retrievedData["Country"]} is ${retrievedData["daysToDeparture"]} days away`
+    document.getElementById("destination").innerText = `My trip to: ${retrievedData["City"]}, ${retrievedData["Country"]}`;
+    document.getElementById("departure-day").innerText = `Departing: ${retrievedData["departureDate"]}`;
+    document.getElementById("days-away").innerText = `${retrievedData["City"]}, ${retrievedData["Country"]} is ${retrievedData["daysToDeparture"]} days away`;
+    if (retrievedData["daysToDeparture"] > 7) {
+        document.getElementById("temp").innerHTML = `High - ${retrievedData["high_temp"]}&#8451, Low - ${retrievedData["low_temp"]}&#8451`;
+    } else {
+        document.getElementById("temp").innerHTML = `${retrievedData["temp"]}&#8451`;
+    };
+    document.getElementById("comment").innerText = `${retrievedData["description"]} throughout the data`;
+    if (retrievedData["CityImg"]) {
+        document.getElementById("dest-imag").src = `${retrievedData["CityImg"]}`;
+    };
+
 }
 
 
